65592ce33f4b6521c5604c558afcdfb8f3b6f7a6
Jean Sossmeier <jean.sossmeier@datassential.com>
2024-04-05 18:34:31 -0300

    Change custom field expression to be able to return multiple operators and field names

--- src/main/java/com/intuit/graphql/filter/visitors/CustomFieldExpression.java
+++ src/main/java/com/intuit/graphql/filter/visitors/CustomFieldExpression.java
@@ -3,17 +3,19 @@ package com.intuit.graphql.filter.visitors;
 import com.intuit.graphql.filter.ast.BinaryExpression;
 import com.intuit.graphql.filter.ast.Operator;
 
+import java.util.List;
+
 public interface CustomFieldExpression {
-
-    String getFieldName();
-
-    Operator getOperator();
 
     String generateExpression(
             BinaryExpression binaryExpression,
             String fieldName,
             String queryString,
             String resolvedOperator);
+
+    List<String> getFieldNames();
+
+    List<Operator> getOperators();
 
     default LogicalOperator getEnclosingLogicalOperator() {
         return LogicalOperator.OR;
